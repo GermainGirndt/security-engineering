81cf15d23739aa1c32abc02b41e806ceca787e3f
Germain Girndt <germaingirndt@outlook.com>
2023-05-07 07:37:18 +0200

    Organized

--- exercise_02/task_01/commands.sh
+++ exercise_02/task_01/commands.sh
@@ -1,5 +1,17 @@
-dd if=/dev/urandom of=testfile bs=1M count=1 #Datei erzeugen mit zufälligen Bits
-ln testfile testlink #Hardlink testlink auf testfile erzeugen
-tar -cf archiv.tar testlink testfile #Das Archiv ist etwas größer als 1MB
-ln -s testfile testlink2
-tar -cf archiv2.tar testlink2 testfile #Das Archiv ist genauso groß wie das erste
+# Datei erzeugen mit zufälligen Bits
+dd if=/dev/urandom of=test_file bs=1M count=1
+
+# Links erzeugen
+ln test_file test_hard_link
+ln -s test_file test_soft_link
+
+
+# Archiv erzeugen
+tar -cf archiv_file_and_hard_link.tar test_file test_hard_link
+tar -cf archiv_file_and_softlink.tar test_file test_soft_link
+
+echo "Comparing archives:"
+ls -la archiv*
+
+echo "Comparing files:"
+ls -la test*
