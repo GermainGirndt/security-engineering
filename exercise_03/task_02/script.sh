c8834652eb80d3ffd7f3779b3ed09a8054dd2367
Germain Girndt <germaingirndt@outlook.com>
2023-05-18 16:09:42 +0200

    Uncommented file

--- exercise_03/task_02/script.sh
+++ exercise_03/task_02/script.sh
@@ -1,6 +1,6 @@
 URL=https://de.wikipedia.org/wiki/Fußball-Bundesliga_2022/23
 
-wget $URL -O page.html
+#wget $URL -O page.html
 
 line_where_table_begins=$(cat page.html | head --lines=500 | egrep -n "<table class.*wikitable sortable.*" | cut -d ":" -f 1)
 line_where_table_ends=$(cat page.html | head --lines=1200 | egrep -n "</th></tr></tbody></table>" | cut -d ":" -f 1)
@@ -14,7 +14,7 @@ formatted_table=$(echo "$table" | sed 's/th/td/g')
 
 lines_with_place_and_teams=$(echo "$formatted_table"  | grep -Ezo '<td style="text-align:center;">.*\d\d?\.\n+</td>''|''<a href="/wiki/[^"]+" title="[^"]+">[^<]+</a>' ) 
 
-echo "$lines_with_place_and_teams" | sed  's/&#x2002;//g' | sed -E 's/<[^>]*>//g'
+echo "$lines_with_place_and_teams" | sed  's/&#x2002;//g' | sed -E 's/<[^>]*>//g' | tr '\n' ' ' 
 
 
 
