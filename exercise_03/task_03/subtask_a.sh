0d44a8256c8ae7a95fdd1b2230c0df01803b034a
Germain Girndt <germaingirndt@outlook.com>
2023-05-23 10:53:51 +0200

    Added error handling

--- exercise_03/task_03/subtask_a.sh
+++ exercise_03/task_03/subtask_a.sh
@@ -4,6 +4,12 @@
 # Store command-line arguments in an array
 args=("$@")
 
+# Check if any arguments were provided
+if [ ${#args[@]} -eq 0 ]; then
+    echo "No arguments provided."
+    exit 1
+fi
+
 # Loop through the array and print each argument
 for arg in "${args[@]}"; do
     echo "Hello $arg"
