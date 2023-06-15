e3c3ec150fd7f6d3c151f6d1b0ca381448baae02
AntonioMetz <131550877+AntonioMetz@users.noreply.github.com>
2023-06-15 14:38:48 +0200

    Create script.sh

---
+++ exercise_05/task_01/script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Password of Steffi.Jones:"
+passwordcracker.sh \$1\$O7v0C21Z\$2FH7ib2Dxtoq6B83qTgON1
+echo "Password of Marco.Reus:"
+passwordcracker.sh \$1\$Jebn3vQ5\$2k..iqxtXNwfsCFAamWCS0
+echo "Password of Almuth.Schult:"
+passwordcracker.sh \$1\$0ngrMRa1\$uXLzWhnrYzmiRM3fi8Nde1
+echo "Password of Manuel.Neuer:"
+passwordcracker.sh \$1\$1aaPttrp\$VoF2rkOyC/tE.DxzQuuIY1
+echo "Password of Birgit.Prinz:"
+passwordcracker.sh \$1\$7ieEwjFr\$T/jwatbzqhLZNVDEfymB41
