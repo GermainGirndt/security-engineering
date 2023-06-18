dcf067b72c02ad42afebcd10e89209ae6652d84b
Germain Girndt <germaingirndt@outlook.com>
2023-06-18 09:11:50 +0200

    Added C starter

---
+++ exercise_04/task_02/compare/run.sh
@@ -0,0 +1,10 @@
+echo "Compiling start my..."
+gcc -o start_compare start_compare.c -lpthread 
+echo "Running start_compare my..."
+./start_compare
+
+
+echo "Compiling start my..."
+gcc -o start_compare_2 start_compare_2.c -lpthread 
+echo "Running start_compare_2 my..."
+./start_compare_2
