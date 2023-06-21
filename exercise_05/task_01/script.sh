12c3442271298ab00e7b8434b5dc16806be165c4
Germain Girndt <germaingirndt@outlook.com>
2023-06-21 21:39:36 +0200

    Added current path to file

--- exercise_05/task_01/script.sh
+++ exercise_05/task_01/script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 echo "Password of Steffi.Jones:" #yacht
-passwordcracker.sh \$1\$O7v0C21Z\$2FH7ib2Dxtoq6B83qTgON1
+./passwordcracker.sh '\$1\$O7v0C21Z\$2FH7ib2Dxtoq6B83qTgON1'
 echo "Password of Marco.Reus:" #unsecure
-passwordcracker.sh \$1\$Jebn3vQ5\$2k..iqxtXNwfsCFAamWCS0
+./passwordcracker.sh \$1\$Jebn3vQ5\$2k..iqxtXNwfsCFAamWCS0
 echo "Password of Almuth.Schult:" #wintering
-passwordcracker.sh \$1\$0ngrMRa1\$uXLzWhnrYzmiRM3fi8Nde1
+./passwordcracker.sh \$1\$0ngrMRa1\$uXLzWhnrYzmiRM3fi8Nde1
 echo "Password of Manuel.Neuer:" #pyramid
-passwordcracker.sh \$1\$1aaPttrp\$VoF2rkOyC/tE.DxzQuuIY1
+./passwordcracker.sh \$1\$1aaPttrp\$VoF2rkOyC/tE.DxzQuuIY1
 echo "Password of Birgit.Prinz:" #flyer
-passwordcracker.sh \$1\$7ieEwjFr\$T/jwatbzqhLZNVDEfymB41
+./passwordcracker.sh \$1\$7ieEwjFr\$T/jwatbzqhLZNVDEfymB41
