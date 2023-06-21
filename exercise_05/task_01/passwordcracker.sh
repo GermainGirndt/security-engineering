7015dbb653ad71e3f014ce8aba793f692fd36198
Germain Girndt <germaingirndt@outlook.com>
2023-06-21 21:36:07 +0200

    Added comments

--- exercise_05/task_01/passwordcracker.sh
+++ exercise_05/task_01/passwordcracker.sh
@@ -1,16 +1,30 @@
 #! /bin/bash
-input=$1
+
+# The script expects an argument ($1) which should be the hashed password.
+input=$1 
+
+# Read each line (word) of the file /usr/share/dict/words.
 while IFS= read -r word
 do
+        # Skip words that start with "-"
         if [ ${word:0:1} != "-" ]
         then
+                # Extract the salt from the input (hashed password).
                 salt=${input:3:11}
+
+                # Hash the current word from the dictionary using the extracted salt.
                 hash=$(openssl passwd -1 -salt $salt $word)
+
+                # If the newly computed hash and the input hash are same, then we found the password.
                 if [ "$hash" == "$input" ]
                 then
+                        # Print the discovered password.
                         echo "The password is $word."
+                        # Exit the script as the password has been found.
                         exit 0
                 fi
         fi
 done < /usr/share/dict/words
+
+# If no match was found in the dictionary, print a message.
 echo "The password has not been found."
