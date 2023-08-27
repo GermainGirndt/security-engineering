955dbbbd41a035a848616aece6884b0ff0794273
Germain Girndt <germaingirndt@outlook.com>
2023-08-27 10:23:57 +0200

    Added coments

--- exercise_02/task_01/commands.sh
+++ exercise_02/task_01/commands.sh
@@ -2,8 +2,8 @@
 dd if=/dev/urandom of=test_file bs=1M count=1
 
 # Links erzeugen
-ln test_file test_hard_link
-ln -s test_file test_soft_link
+ln test_file test_hard_link # same size as test_file
+ln -s test_file test_soft_link # much smaller as test_file
 
 
 # Archiv erzeugen
@@ -12,8 +12,9 @@ tar -cf archiv_file_and_softlink.tar test_file test_soft_link
 
 
 echo "Comparing files:"
-ls -la test*
+ls -la test* # same size
 
 # No difference in size
 echo "Comparing archives:"
-ls -la archiv*
+ls -la archiv* # same size
+
